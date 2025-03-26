137e1bab2553c444ffcde3968e3685f2af752eb4
Nik Linders <info@eleven59.nl>
2025-03-26 22:24:09 +0100

    Fixed error in loading payment methods

--- src/Models/PaymentProvider.php
+++ src/Models/PaymentProvider.php
@@ -32,7 +32,6 @@ class PaymentProvider extends \Eleven59\BackpackShop\PaymentProvider
     public static function getPaymentMethods(array $options = []) :array
     {
         $options = array_merge($options, [
-            "sequenceType" => "first",
             "locale" => config('eleven59.backpack-shop-mollie.locale', 'nl_NL'),
 //            "include" => "issuers",
         ]);
